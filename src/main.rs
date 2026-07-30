73700a299087b84d7314084d6adff72a2e9c915b
rangeset <2432647223@qq.com>
2026-07-30 09:19:43 +0800

    init(project): initialize the project
    initialize the project by Cargo
    create main and Cargo configure file

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// ==============================================================
+// Copyright (c) 2026 Team ISET
+// Licensed under the MIT.
+// https://github.com/RAT-ISET/jmap-server
+// ==============================================================
+// Path /src/main.rs
+// Main of the project.
+
+fn main() {
+    println!("Hello, world!");
+}
